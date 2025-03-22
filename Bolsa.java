cc11f161852a6e54e92241f1333428836f9f9b7c
Josjo33 <joshuahugo131@gmail.com>
2025-03-22 14:09:44 -0500

    fix: corregir error en la inicialización de lista en Bolsa

--- Bolsa.java
+++ Bolsa.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Bolsa<T> implements Iterable<T> {
-    private int tope;
+    private ArrayList<T> lista; // Lista para almacenar los objetos
+    private int tope; // Límite de elementos en la bolsa
 
     public Bolsa(int tope) {
         this.tope = tope;
+        this.lista = new ArrayList<>(); // Se inicializa la lista
     }
 
     public void add(T objeto) {
-        if (lista.size() < tope) {
+        if (lista.size() < tope) { // Se verifica que no haya excedido el límite
             lista.add(objeto);
         } else {
             throw new RuntimeException("No caben más elementos en la bolsa");
